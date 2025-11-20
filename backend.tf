7f7c3faf926125a144dad40cdba3948520d857de
Tommy <tommy@Tommys-MBA.local>
2025-11-19 18:57:59 -0600

    chore: Clean up sensitive files and disable CI workflow

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "kodekloud-lab-state-1763599383"
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "kodekloud-lab-locks-1763599383"
-    encrypt        = true
-  }
-}
