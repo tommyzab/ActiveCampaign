d963a845cfbe830e8ce51c8730503fd8fbecaacd
Tommy <tommy@Tommys-MBA.local>
2025-11-19 19:11:24 -0600

    ...

---
+++ modules/eks/variables.tf
@@ -0,0 +1,18 @@
+variable "cluster_name" { 
+    type = string 
+    }
+variable "vpc_id" { 
+    type = string 
+    }
+
+variable "private_subnet_ids" { 
+    type = list(string) 
+    }
+
+variable "environment" { 
+    type = string 
+    }
+
+variable "project_name" { 
+    type = string 
+    }
