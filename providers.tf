394b499e708917a55846d15ad2ae6cd4f7eb2110
Tommy <tommy@Tommys-MBA.local>
2025-11-26 15:45:00 -0600

    feat: enable Okta provider for identity integration

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.0"
   required_providers {
     aws = { source = "hashicorp/aws", version = "~> 5.0" }
-    # okta = { source = "okta/okta", version = "~> 4.0" }
+    okta = { source = "okta/okta", version = "~> 4.0" }
   }
 }
 
@@ -10,8 +10,8 @@ provider "aws" {
   region = var.region
 }
 
-# provider "okta" {
-#   org_name  = var.enable_identity ? var.okta_org_name : null
-#   base_url  = "okta.com"
-#   api_token = var.enable_identity ? var.okta_api_token : null
-# }
+provider "okta" {
+  org_name  = var.enable_identity ? var.okta_org_name : null
+  base_url  = "okta.com"
+  api_token = var.enable_identity ? var.okta_api_token : null
+}
