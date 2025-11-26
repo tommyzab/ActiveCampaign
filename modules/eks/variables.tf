ea70a88219a04481dc5a65296d5aaa19a72fd46f
Tommy <tommy@Tommys-MBA.local>
2025-11-26 15:44:58 -0600

    refactor: make cluster and node group IAM role ARNs required variables

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -18,13 +18,11 @@ variable "project_name" {
     }
 
 variable "cluster_iam_role_arn" {
-  description = "ARN of existing IAM role to use for EKS cluster. If not provided, module will create one (requires iam:PassRole permission)."
+  description = "ARN of existing IAM role to use for EKS cluster. Required - must be provided."
   type        = string
-  default     = null
 }
 
 variable "node_group_iam_role_arn" {
-  description = "ARN of existing IAM role to use for EKS node groups. If not provided, module will create one. For lab environments, you may use the same role as cluster_iam_role_arn."
+  description = "ARN of existing IAM role to use for EKS node groups. Required - must be provided."
   type        = string
-  default     = null
 }
